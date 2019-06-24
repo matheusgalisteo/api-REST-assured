e59de8aa7bd2adda18eed86accf83e684452407e
matheusgalisteo <matheus.galisteo@uhuu.com>
2019-06-23 21:29:22 -0300

    Fim da aula uso do XML

--- src/main/java/br/ce/mvgalisteo/CursoRest/UserXMLTest.java
+++ src/main/java/br/ce/mvgalisteo/CursoRest/UserXMLTest.java
@@ -1,11 +1,27 @@
 package br.ce.mvgalisteo.CursoRest;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 import org.junit.Test;
+import io.restassured.RestAssured.*;
 
 public class UserXMLTest {
 	@Test
 	public void devoTrabalharComXML(){
-		
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML/3")
+		.then()
+			.statusCode(200)
+		.body("user.name", is("Ana Julia"))
+		//para XML que usa atributos é necessário referencia-los com o @, sendo um atributo
+		.body("user.@id", is("3"))
+		.body("user.filhos.name.size()", is(2))
+		.body("user.filhos.name[0]", is("Zezinho"))
+		.body("user.filhos.name[1]", is("Luizinho"))
+		.body("user.filhos.name", hasItem("Luizinho"))
+		.body("user.filhos.name", hasItems("Luizinho","Zezinho"))
+		;
 	}
 
 }

87429f0aa8901f6cfc572fe187f5e4fb05575bbe
Matheus <matheus.galisteo@uhuu.com>
2019-07-09 12:16:41 -0300

    Verbos rest, inclusão classe VerbosTest.java

--- src/main/java/br/ce/mvgalisteo/CursoRest/VerbosTest.java
+++ src/main/java/br/ce/mvgalisteo/CursoRest/VerbosTest.java
@@ -8,7 +8,10 @@ import org.hamcrest.Matchers;
 import org.hamcrest.Matchers.*;
 import org.junit.Test;
 
+import com.sun.istack.internal.NotNull;
+
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 public class VerbosTest {
 	@Test
@@ -22,10 +25,120 @@ public class VerbosTest {
 		.then()
 			.log().all()
 			.statusCode(201)
-			//.body("id", is(notNullValue()))
+			//.body("id", is(NotNullValue()))
 			.body("name", is("Jose"))
-			.body("name", is(50))
+			.body("age", is(50))
 		;
 	}
+	@Test
+	public void naoDeveSalvarUsuarioSemNome() {
+		given()
+			.log().all() // ver a requisição que estou mandando
+			.contentType("application/json") // dizendo que meu objeto deve ser interpretado como objeto json
+				.body("{\"age\": 50}")
+				.when()
+					.post("https://restapi.wcaquino.me/users")
+				.then()
+				.log().all()
+				.statusCode(400)
+				.body("error", is("Name é um atributo obrigatório"))
+				//.body("id", is(nullValue()))
+				//.body("name", is("Jose")).body("age", is(50));
+				;
+	
+	}
+	@Test
+	public void deveSalvarUsuarioXML(){
+		given()
+			.log().all() //ver a requisição que estou mandando
+			.contentType("application/xml") //.contentType(ContentType.XML) <- mais indicado//dizendo que meu objeto deve ser interpretado como objeto json
+			.body("<user><name>Jose</name><age>50</age></user>")
+		.when()
+			.post("https://restapi.wcaquino.me/usersXML")
+		.then()
+			.log().all()
+			.statusCode(201)
+			//.body("id", is(NotNullValue()))
+			.body("user.name", is("Jose"))
+			.body("user.age", is("50"))
+		;
+	}
+	@Test
+	public void deveAlterarUsuario(){
+		given()
+			.log().all() //ver a requisição que estou mandando
+			.contentType("application/json") //dizendo que meu objeto deve ser interpretado como objeto json
+			.body("{\"name\": \"Usuario Alterado\",\"age\": 80}")
+		.when()
+			.put("https://restapi.wcaquino.me/users/1") //Put para realizar alteração
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuario Alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
+	@Test
+	public void deveCustomizarURL(){
+		given()
+			.log().all() //ver a requisição que estou mandando
+			.contentType("application/json") //dizendo que meu objeto deve ser interpretado como objeto json
+			.body("{\"name\": \"Usuario Alterado\",\"age\": 80}")
+		.when()
+			.put("https://restapi.wcaquino.me/{entidade}/{userId}", "users", "1") //Put para realizar alteração
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuario Alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
+	@Test
+	public void deveCustomizarURLParteII(){
+		given()
+			.log().all() //ver a requisição que estou mandando
+			.contentType("application/json") //dizendo que meu objeto deve ser interpretado como objeto json
+			.body("{\"name\": \"Usuario Alterado\",\"age\": 80}")
+			.pathParam( "entidade", "users")
+			.pathParam( "userId", "1")			
+		.when()
+			.put("https://restapi.wcaquino.me/{entidade}/{userId}") //Put para realizar alteração
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuario Alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
+	@Test
+	public void deveRemoverUsuario(){
+		given()
+			.log().all()
+		.when()
+			.delete("https://restapi.wcaquino.me/users/1")
+		.then()
+			.log().all()
+			.statusCode(204)
+		;
+	}
+	@Test
+	public void naoDeveRemoverUsuarioInexistente(){
+		given()
+			.log().all()
+		.when()
+			.delete("https://restapi.wcaquino.me/users/1000")
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("error", is("Registro inexistente"))
+		;
+	}
+	
 }
 
